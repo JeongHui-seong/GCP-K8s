d8875569e3e9f734f1bdcd67498f4b73aa81e0c9
JeongHui-seong <jgmltjd99@naver.com>
2026-06-18 13:52:29 +0900

    Grafana & Prometheus 연동 및 배포 완료

--- terraform/03. compute/outputs.tf
+++ terraform/03. compute/outputs.tf
@@ -7,15 +7,15 @@ gcp_project = ${var.gcp_project_id}
 gcp_zone = ${var.gcp_zone}
 
 [k8s_master]
-${google_compute_instance.k8s_master.name} ansible_host=${google_compute_instance.k8s_master.name}
+${google_compute_instance.k8s_master.name} ansible_host=${google_compute_instance.k8s_master.name} private_ip=${google_compute_instance.k8s_master.network_interface[0].network_ip}
 
 [k8s_workers]
 %{ for vm in google_compute_instance.k8s_workers ~}
-${vm.name} ansible_host=${vm.name}
+${vm.name} ansible_host=${vm.name} private_ip=${vm.network_interface[0].network_ip}
 %{ endfor ~}
 
 [monitoring]
-${google_compute_instance.monitoring_vm.name} ansible_host=${google_compute_instance.monitoring_vm.name}
+${google_compute_instance.monitoring_vm.name} ansible_host=${google_compute_instance.monitoring_vm.name} private_ip=${google_compute_instance.monitoring_vm.network_interface[0].network_ip}
 
 [k8s_cluster:children]
 k8s_master
